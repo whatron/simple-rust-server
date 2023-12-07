899cf3bfa505f41d33baaa7c834e372c03ab4e06
whatron <94902703+whatron@users.noreply.github.com>
2023-12-07 21:22:24 +0800

    Initial Commit

---
+++ src/http/response.rs
@@ -0,0 +1,30 @@
+use super::StatusCode;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::net::TcpStream;
+use std::io::{Write, Result as IOResult};
+
+#[derive(Debug)]
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Response { status_code, body }
+    }
+    pub fn send(&self, stream: &mut impl Write) -> IOResult<()> {
+        let body = match &self.body {
+            Some(b)=> b,
+            None => " ",
+        };
+
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n {}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body,
+        )
+    }
+}
